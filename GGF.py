ec6a161729fb3ec0cd6367957d81f79b47a93536
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2022-07-22 04:52:35 +0000

    Update GGF.py

--- GGF.py
+++ GGF.py
@@ -5,19 +5,27 @@ import sys
 import concurrent.futures
 import ctypes
 import time
+from colorama import Fore
+
+WRO = '\33[91m'
+NG = '\33[0m'
+PO = '\33[34m'
+RT = '\33[0m'
 
 libgcc_s = ctypes.CDLL('libgcc_s.so.1')
 
 start = time.perf_counter()
 
 target = sys.argv[1]
-
+print("Created by: SickAndTired")
+print("")
 def scan(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.2)
     try:
         s.connect((target, port))
-        print(f"{target}:{port}")
+        p = (PO + f"{port}" + RT)
+        print("OPEN"+f"[{p}]")
         s.close()
     except:
         pass
